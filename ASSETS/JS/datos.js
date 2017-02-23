53b8a258d98b7b2a43ce2f2ecea3e4d82027c3e7
ely211 <gonzalesm405@gmail.com>
2017-02-23 16:59:18 -0500

    ficha

---
+++ ASSETS/JS/datos.js
@@ -0,0 +1,20 @@
+function imprime(){
+  var resultado = document.getElementById("reporte");
+  var ingNombre = document.getElementById("name").value;
+  var ingApellido = document.getElementById("lastName").value;
+  var ingDNI = document.getElementById("nDNI").value;
+  var ingDireccion = document.getElementById("direction").value;
+  var totDatos = document.getElementsByClassName("datos");
+  if(totDatos[0].value.length > 0 && totDatos[1].value.length > 0 && totDatos[2].value.length > 0 && totDatos[3].value.length > 0){
+    resultado.innerHTML= "<ul>"
+                          +"<li>Nombre:  "+ingNombre  +"</li>"
+                          +"<li>Apellido:"+ingApellido+"</li>"
+                          +"<li>DNI:     "+ingDNI     +"</li>"
+                          +"<li>Dirección:"+ingDireccion+"</li>"
+                        +"</ul>";
+  }
+  //document.getElementById("formulario").reset();
+}
+function limpiar(){
+  document.getElementById("formulario").reset();
+}
